501befcd4693c9f10ff484f90c247d886dc85163
Muflone <webreg@muflone.com>
2022-06-12 21:22:30 +0200

    Bumped version number

--- awaitable/constants.py
+++ awaitable/constants.py
@@ -19,7 +19,7 @@
 ##
 
 APP_NAME = 'Awaitable'
-APP_VERSION = '0.1.0'
+APP_VERSION = '0.2.0'
 APP_DESCRIPTION = 'A decorator to asynchronously execute synchronous functions'
 APP_ID = 'awaitable.muflone.com'
 APP_URL = 'https://www.muflone.com/awaitable/'
